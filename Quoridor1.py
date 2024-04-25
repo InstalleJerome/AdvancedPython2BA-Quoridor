dc08ac94b412af82ce90aa7a720e53c20481db52
Installé Jérôme <22237@ecam.be>
2024-04-25 09:35:48 +0200

    Add vertical moves

--- Quoridor1.py
+++ Quoridor1.py
@@ -30,7 +30,15 @@ print(json.loads(res))
 
 sconnect.close()
 
-    
+def vertical_move(p_init,indice,player):
+    if player==True:
+        if indice==float(1):
+            p_init=[p_init[0]-4,p_init[1]]
+    if indice==float(0):
+        if player==True:
+            p_init=[p_init[0]+4,p_init[1]]        
+    return p_init
+
 server=socket.socket()
 server.settimeout(0.5)
 
@@ -55,7 +63,38 @@ while True:
             while x<len(response_pong):
                 x += client.send(response_pong)
         if req["request"]=="play":
-            print(req["state"]["board"])
+            indice=0
+            my_position=[0,0]
+            if req["state"]["players"][0]==nom:
+                my_indice=float(0)
+                other_indice=float(1)
+            else:
+                my_indice=float(1)
+                other_indice=float(0)
+            for i in range(len(req["state"]["board"])):
+                for j in range(len(req["state"]["board"][i])):
+                    if req["state"]["board"][i][j]==indice:
+                        position=[i,j]
+            if indice==float(0):
+                if req["state"]["board"][position[0]+1][position[1]]==float(3):
+                    move={
+                        "type":"pawn",
+                        "position": [vertical_move(position,indice)],
+                    }
+            if indice==float(1):
+                if req["state"]["board"][position[0]-1][position[1]]==float(3):
+                    move={
+                        "type":"pawn",
+                        "position": [vertical_move(position,indice)],
+                    }
+            response_move={
+                    "response": "move",
+                    "move": move,
+                    "message": "Fun message"
+                    }
+            rep_move=json.dumps(response_move).encode()
+            while x<len(rep_move):
+                x+=client.send(rep_move)
         client.close()
     except socket.timeout:
         pass
